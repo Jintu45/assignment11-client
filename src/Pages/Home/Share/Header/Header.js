bc94eb3882664e88e2e1820d011533dd96dfb476
Zintu paul <jintupaul45@gmail.com>
2022-11-10 19:02:16 +0600

    some mistake solve

--- src/Pages/Home/Share/Header/Header.js
+++ src/Pages/Home/Share/Header/Header.js
@@ -11,20 +11,14 @@ const Header = () => {
         .catch(err => alert(err))
     }
     return (
-        <div className="navbar h-28 w-full">
-                    <div className="navbar-start">
-                        <div className="dropdown">
-                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                        </label>
-                        
-                        </div>
-                        <img src={logo} alt="" />
-                        <a className="btn btn-ghost normal-case text-2xl">SK Photography</a>
-                    </div>
-                    <div className="navbar-center hidden lg:flex">
-                        <ul className="menu menu-horizontal p-0">
-                            <Link  className='text text-stone-600 font-semibold  text mr-5' to='/'>Home</Link>
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                </label>
+                <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                <Link  className='text text-stone-600 font-semibold  text mr-5' to='/'>Home</Link>
                             {
                                 user?.email ?
 
@@ -35,10 +29,18 @@ const Header = () => {
 
                             }
                             <Link to='/blog' className='text text-stone-600 font-semibold  text mr-5'>Blogs</Link>
-                        </ul>
-                    </div>
-                   <div className="navbar-end">
-                    {
+                </ul>
+                </div>
+                <img src={logo} alt="" />
+                        <a className="btn btn-ghost normal-case text-sm lg:text-2xl">SK Photography</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal p-0">
+                
+                </ul>
+            </div>
+            <div className="navbar-end">
+            {
                         user?.email ? 
                        <> 
                             <button onClick={handleLogOut} className='btn btn-dark py-2 px-6'>LogOut</button>
@@ -53,8 +55,8 @@ const Header = () => {
                         </Link>
                         </>
                     }
-                    </div>
-        </div>
+            </div>
+            </div>
     );
 };
 
